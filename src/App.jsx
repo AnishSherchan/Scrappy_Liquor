1e909adec08d05ac8687f0c3822e0ac0b6428824
AnishSherchan <sherchananish11@gmail.com>
2023-05-30 02:03:09 +0545

    halo

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ function App() {
   return (
     <div>
       <Navbar />
-      <div className=" relative h-screen w-full bg-[url('/src/assets/mob.png')] lg:bg-[url('/src/assets/Main.png')] bg-no-repeat bg-center lg:bg-fixed bg-cover">
-        <div className="absolute top-[34%] lg:top-[27%] left-[12%]">
-          <h1 className="hero text-white text-[30px] lg:text-[40px] tracking-wider">
-            Welcome to Scrappy
-          </h1>
-          <h2 className="hero text-white mt-3 mb-9 text-[22px] lg:text-[28px] tracking-wider">
-            Get your Whiskey today.
-          </h2>
-          <Primary title="Get your Premium Liquors." />
+      <div className=" relative h-screen w-full bg-[url('https://cdn.shopify.com/s/files/1/0025/8555/9110/files/Header-Image_1_1080x.png?v=1675715280')]  lg:bg-[url('/src/assets/Main.png')] bg-no-repeat bg-center lg:bg-fixed bg-cover">
+        <div className=" bg-opacity-25 lg:bg-opacity-0 h-full bg-black">
+          <div className="absolute top-[34%] lg:top-[27%] left-[12%]">
+            <h1 className="hero text-white text-[30px] lg:text-[40px] tracking-wider">
+              Welcome to Scrappy
+            </h1>
+            <h2 className="hero text-white mt-3 mb-9 text-[22px] lg:text-[28px] tracking-wider">
+              Get your Whiskey today.
+            </h2>
+            <Primary title="Get your Premium Liquors." />
+          </div>
         </div>
       </div>
       <div className="h-screen">
