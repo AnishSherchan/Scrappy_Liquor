0f3d4afaaa4d8080a88e9a3cec270ae17964cdcc
AnishSherchan <sherchananish11@gmail.com>
2023-05-31 00:28:48 +0545

    Arrow

--- src/components/Content/OurProducts.jsx
+++ src/components/Content/OurProducts.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
 import ProductCard from "../ProductCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -7,6 +8,8 @@ import "slick-carousel/slick/slick-theme.css";
 
 const OurProducts = ({ productData, onLoad }) => {
   const [slidesToShow, setSlidesToShow] = useState(4);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 768) {
@@ -26,6 +29,15 @@ const OurProducts = ({ productData, onLoad }) => {
     speed: 500,
     slidesToShow,
     slidesToScroll: 1,
+    prevArrow:
+      windowWidth <= 768 ? (
+        <Icon
+          icon="icon-park-outline:left"
+          className=" text-primary h-[16px] w-[8px]"
+        />
+      ) : null,
+    nextArrow:
+      windowWidth <= 768 ? <Icon icon="icon-park-outline:right" /> : null,
   };
 
   return (
