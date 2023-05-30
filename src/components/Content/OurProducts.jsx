abb2563fb4b31d68722ffa36f661325b60c2ccb2
AnishSherchan <sherchananish11@gmail.com>
2023-05-30 13:25:47 +0545

    h

--- src/components/Content/OurProducts.jsx
+++ src/components/Content/OurProducts.jsx
@@ -1,13 +1,14 @@
 const OurProducts = () => {
   return (
     <div className=" px-8 lg:px-16 my-8  flex flex-col gap-12">
-      <h5 className="text-center text-[20px] font-extrabold tracking-normal lg:text-[22px] decoration-2 underline decoration-primary underline-offset-4">
+      <h5 className="text-center text-[22px] font-extrabold tracking-normal lg:text-[22px] decoration-2 underline decoration-primary underline-offset-4">
         OUR PRODUCTS
       </h5>
       <section>
-        <h6 className="underline font-black text-[18px] lg:text-[20px] decoration-2 decoration-primary underline-offset-4">
+        <h6 className="underline font-black text-[20px] lg:text-[20px] decoration-2 decoration-primary underline-offset-4">
           Our Whiskey
         </h6>
+        <p>Halo</p>
       </section>
     </div>
   );
