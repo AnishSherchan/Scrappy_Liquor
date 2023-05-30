0087f1ba1a5d96cebe263870c45fabcbd369e5e4
AnishSherchan <sherchananish11@gmail.com>
2023-05-30 23:26:08 +0545

    Slider added

--- src/components/Content/OurProducts.jsx
+++ src/components/Content/OurProducts.jsx
@@ -1,7 +1,33 @@
 /* eslint-disable */
+import { useState, useEffect } from "react";
 import ProductCard from "../ProductCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const OurProducts = ({ productData }) => {
+  const [slidesToShow, setSlidesToShow] = useState(4);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setSlidesToShow(1);
+      } else if (window.innerWidth <= 1024) {
+        setSlidesToShow(2);
+      } else {
+        setSlidesToShow(3);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow,
+    slidesToScroll: 1,
+  };
   return (
     <div className=" px-8 lg:px-16 my-8  flex flex-col gap-12">
       <h5 className="text-center text-[20px] font-extrabold tracking-normal lg:text-[22px] decoration-2 underline decoration-primary underline-offset-4">
@@ -11,10 +37,13 @@ const OurProducts = ({ productData }) => {
         <h6 className="underline mb-10 font-black text-[18px] lg:text-[20px] decoration-2 decoration-primary underline-offset-4">
           Our Whiskey
         </h6>
-        <div className=" flex flex-wrap justify-evenly items-center">
-          {productData.map((item, index) => (
-            <ProductCard productData={item} key={index} />
-          ))}
+
+        <div className=" p-4 ">
+          <Slider {...settings} autoplay={true} autoplaySpeed={3000}>
+            {productData.map((item, index) => (
+              <ProductCard productData={item} key={index} />
+            ))}
+          </Slider>
         </div>
       </section>
     </div>
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -3,7 +3,7 @@ const ProductCard = ({ productData }) => {
   console.log(productData);
 
   return (
-    <div className="product_item py-6 px-6 w-[270px] h-full rounded-[20px]">
+    <div className="product_item cursor-pointer py-6 px-6 w-[270px] h-full rounded-[20px]">
       <div className=" w-full flex justify-center items-center object-cover h-[280px] mb-4">
         <img src={productData.img} alt={productData.category} />
       </div>
