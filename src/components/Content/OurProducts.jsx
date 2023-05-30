22e824e9b0eea73bc2098aaaad5de7a3baa8eaa5
AnishSherchan <sherchananish11@gmail.com>
2023-05-30 12:58:39 +0545

    h

--- src/components/Content/OurProducts.jsx
+++ src/components/Content/OurProducts.jsx
@@ -1,6 +1,6 @@
 const OurProducts = () => {
   return (
-    <div className="px-4 lg:px-16 my-8">
+    <div className=" px-8 lg:px-16 my-8">
       <h5 className="text-center text-[20px] lg:text-[22px] decoration-2 underline decoration-primary underline-offset-4">
         OUR PRODUCTS
       </h5>
